7cc79e710f21c87fdd6c8f392c5c988cd166689f
CodingIran <CodingIran@gmail.com>
2023-02-28 16:59:58 +0800

    mac 重复点击间隔

--- flutter_web_auth_2/macos/Classes/FlutterWebAuth2Plugin.swift
+++ flutter_web_auth_2/macos/Classes/FlutterWebAuth2Plugin.swift
@@ -5,6 +5,7 @@ import SafariServices
 @available(OSX 10.15, *)
 public class FlutterWebAuth2Plugin: NSObject, FlutterPlugin {
     var lastSession: ASWebAuthenticationSession?
+    var lastTimeStamp: TimeInterval?
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "flutter_web_auth_2", binaryMessenger: registrar.messenger)
         let instance = FlutterWebAuth2Plugin()
@@ -13,6 +14,14 @@ public class FlutterWebAuth2Plugin: NSObject, FlutterPlugin {
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         if call.method == "authenticate" {
+            if let lastTimeStamp = lastTimeStamp {
+                let currentTimeStamp = Date.timeIntervalSinceReferenceDate
+                if currentTimeStamp - lastTimeStamp < 0.2 {
+                    result(FlutterError(code: "CANCELED", message: "User canceled login", details: nil))
+                    return
+                }
+            }
+
             let url = URL(string: (call.arguments as! [String: AnyObject])["url"] as! String)!
             let callbackURLScheme = (call.arguments as! [String: AnyObject])["callbackUrlScheme"] as! String
             let preferEphemeral = (call.arguments as! [String: AnyObject])["preferEphemeral"] as? Bool
@@ -22,8 +31,9 @@ public class FlutterWebAuth2Plugin: NSObject, FlutterPlugin {
             lastSession = nil
 
             var keepMe: Any?
-            let completionHandler = { (url: URL?, err: Error?) in
+            let completionHandler = { [weak self] (url: URL?, err: Error?) in
                 keepMe = nil
+                self?.lastTimeStamp = nil
 
                 if let err = err {
                     if case ASWebAuthenticationSessionError.canceledLogin = err {
@@ -53,6 +63,7 @@ public class FlutterWebAuth2Plugin: NSObject, FlutterPlugin {
 
             session.start()
             keepMe = session
+            lastTimeStamp = Date.timeIntervalSinceReferenceDate
             lastSession = session
         } else {
             result(FlutterMethodNotImplemented)
