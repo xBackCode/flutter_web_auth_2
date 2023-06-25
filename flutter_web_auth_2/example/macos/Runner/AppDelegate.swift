a84874d80211ad720ed063464b8891224155ffef
CodingIran <CodingIran@gmail.com>
2023-06-25 15:23:40 +0800

    swizzle AppDelegate applicationDidFinishLaunching to auto addURLSchemeEvent

--- flutter_web_auth_2/example/macos/Runner/AppDelegate.swift
+++ flutter_web_auth_2/example/macos/Runner/AppDelegate.swift
@@ -3,7 +3,12 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationDidFinishLaunching(_ notification: Notification) {
+        debugPrint("applicationDidFinishLaunching")
+        super.applicationDidFinishLaunching(notification)
+    }
+
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 }
